3f7a4782f9adb0e3a145d7b10a71f84fb1a6a7e1
neandercode <josephjones555@yahoo.com>
2024-10-06 22:00:32 -0400

    learning data structures and algorithms

--- bleh.js
+++ bleh.js
@@ -7,3 +7,21 @@ function myFunction() {
     button.innerHTML = text;
   }
 }
+
+function linearSearch(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+let numbers = [10, 20, 30, 40, 50];
+let target = 30;
+let result = linearSearch(numbers, target);
+if (result !== -1) {
+  console.log(`found at index ${result}`);
+} else {
+  console.log("not found");
+}
